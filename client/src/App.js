862740d1fc8531177c9e43eddad3038d3b200bb4
+ <+>
2023-03-27 17:21:15 +0300

    Made component Folder with the required files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router';
 import './App.css';
+import NavBar from "./Components/NavBar"
+import HomePage from "./Components/HomePage"
+import Login from "./Components/Login"
+import Register from "./Components/Register"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+      <Route exact path="/" element={<HomePage/>} />
+      <Route exact path="/login" element={<Login/>} />
+      <Route exact path="/register" element={<Register/>} />
+      </Routes>
     </div>
   );
 }
