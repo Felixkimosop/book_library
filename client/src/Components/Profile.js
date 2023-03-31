b78c2441281bddc944b846283840e477582c7203
+John <+>
2023-03-31 11:05:04 +0300

    updated components

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,39 +1,183 @@
-// import React, { useState } from 'react';
-
-// const UserProfilePage = () => {
-//   const [bookCollection, setBookCollection] = useState([
-//     { id: 1, title: 'Book 1', author: 'Author 1', liked: false },
-//     { id: 2, title: 'Book 2', author: 'Author 2', liked: true },
-//     { id: 3, title: 'Book 3', author: 'Author 3', liked: false },
-//   ]);
-
-//   const handleRemoveBook = (bookId) => {
-//     const updatedBookCollection = bookCollection.filter((book) => book.id !== bookId);
-//     setBookCollection(updatedBookCollection);
-//   };
-
-//   const handleLikeBook = (bookId) => {
-//     const updatedBookCollection = bookCollection.map((book) =>
-//       book.id === bookId ? { ...book, liked: !book.liked } : book
-//     );
-//     setBookCollection(updatedBookCollection);
-//   };
-
-//   return (
-//     <div>
-//       <h2>User Profile Page</h2>
-//       <div>
-//         <h3>Book Collection</h3>
-//         {bookCollection.map((book) => (
-//           <div key={book.id}>
-//             <p>{book.title} by {book.author}</p>
-//             <button onClick={() => handleRemoveBook(book.id)}>Remove from Collection</button>
-//             <button onClick={() => handleLikeBook(book.id)}>{book.liked ? 'Unlike' : 'Like'}</button>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UserProfilePage;
+import React, { useState, useContext} from 'react'
+import { AuthContext } from './context/AuthContext';
+import { EventsContext } from './context/EventContext';
+function Profile() {
+  const { user } = useContext(AuthContext);
+  const { events } = useContext(EventsContext)
+  console.log(user.first_name)
+  const [first_name, setFirstName] = useState('user.first_name');
+  const [last_name, setLastName] = useState(user.last_name);
+  const [phone, setPhone] = useState(user.phone);
+  const [email, setEmail] = useState(user.email);
+    const [image, setImage] = useState();
+  const level = sessionStorage.getItem("level");
+  const [event, setEvent] = useState('')
+  console.log(event)
+  function showDetail(id) {
+    fetch(`http://localhost:3000/events/${id}`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${sessionStorage.token}`
+      }
+     })
+    .then(res => res.json())
+    .then(response => {
+      setEvent(response)
+  })
+}
+  function handleDelete() {
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+    return (
+        <div style={{ marginTop: "10vh" }}>
+            <div className='container-md'>
+                <div>
+                    <div className='row'>
+                        <div className='col'>
+                            <h5>My Profile</h5>
+                            <div className='data'>
+                          <form onSubmit={handleSubmit}>
+  <div>
+    <label htmlFor="first-name">First Name</label><br/>
+    <input
+      type="text"
+      id="first-name"
+      name="first-name"
+      onChange={(e) => setFirstName(e.target.value)}
+                        value={first_name}
+                        placeholder={user.first_name}
+    />
+  </div>
+  <div>
+    <label htmlFor="last-name">Last Name</label><br/>
+    <input
+      type="text"
+      id="last-name"
+      name="last-name"
+                        value={last_name}
+                        onChange={(e) =>
+                          setLastName( event.target.value )
+                        }
+    />
+  </div>
+  <div>
+    <label htmlFor="email">Email</label><br/>
+    <input
+      type="text"
+      id="email"
+      name="email"
+                        onChange={(e) =>
+                          setEmail(event.target.value)}
+      value={email}
+    />
+  </div>
+  <div>
+    <label htmlFor="phone">Phone</label><br/>
+    <input
+      type="tel"
+      id="phone"
+      name="phone"
+      onChange={(e) => setPhone(e.target.value)}
+      value={phone}
+    />
+  </div>
+  <div>
+    <label htmlFor="image">Image</label><br/>
+    <input
+      type="file"
+      id="image"
+                        name="image"
+                        value={image}
+      onChange={(e) => setImage(e.target.files[0])}
+    />
+  </div>
+  <div className='d-flex gap-4'>
+    <button type='submit'>Update</button>
+    <button type='button' onClick={handleDelete}>Delete</button>
+  </div>
+</form>
+                            </div>
+                        </div>
+                        <div className='col'>
+                            {level === "admin" ?
+                                    (
+                                        <>
+                      <div>
+                        <div className='d-flex'>
+                          <h5>EVENTS</h5>
+                          <button>Create new event</button>
+                        </div>
+                        <div>
+                          {events && events.map((event, index) => (
+                            <card className='d-flex gap-4' style={{margin:"7px"}} key={index}>
+                            <img style={{width:"200px", height:"120px"}} src={event.poster_url} alt=''/>
+                            <div>
+                                <p>{event.event_name}</p>
+                                <p>{event.event_date} {event.start_time}</p>
+                                <button type="button" onClick={(e) => showDetail(event.id)} class="btn btn-primary" data-toggle="modal" data-target="#bookModal"><i className='bi bi-pencil-square'></i></button>
+                                <button><i className='bi bi-trash'></i></button>
+                            </div>
+                            </card>
+                             ))}
+                        </div>
+                                        </div>
+                                        </>
+                                    ) : (
+                                        <>
+                      <div>
+                        <h5>my bookings</h5>
+                      </div> 
+                                        </>
+                                    )
+                            }
+                        </div>
+            </div>
+            <div class="modal fade" id="bookModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+              <form >
+                <div className='row'>
+                  <div className='col'></div>
+                   <div className='col'></div>
+                </div>
+                <h5>Ticket: </h5>
+           <h5>Tickets left: </h5>
+      <label htmlFor="amount">Amount</label>
+      <input type="text" id="amount"  />
+      <label htmlFor="currency">Currency</label>
+      <select id="currency" >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="KES">KES</option>
+        <option value="CAD">CAD</option>
+      </select>
+      <label htmlFor="description">Description</label>
+      <input type="text" id="description"  />
+      <button type="submit">Pay Now</button> 
+      <button type="button" >Cancel</button>
+    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">BOOK</button>
+        <button type="button" class="btn btn-primary">Cancel</button>
+      </div>
+    </div>
+  </div>
+</div>
+                </div>
+            </div>
+    </div>
+  )
+}
+export default Profile
--- client/src/Components/User.jsx
+++ client/src/Components/User.jsx
@@ -1,84 +1,3 @@
-// import { useState, useEffect } from 'react';
-// // import { Link } from 'react-router-dom';
-// import { FaUserCircle } from 'react-icons/fa';
-// import Swal from 'sweetalert2';
-// import './User.css';
-
-// function User() {
-//   const [currentUser, setCurrentUser] = useState(null);
-//   const [books, setBooks] = useState([]);
-
-//   useEffect(() => {
-//     // fetch the current user's data from the API
-//     fetch('/loggedin')
-//       .then(response => response.json())
-//       .then(data => console.log(data))
-//       .catch(error => console.error(error));
-//   }, []);
-
-//   useEffect(() => {
-//     // fetch the user's collection of books from the API
-//     if (currentUser) {
-//       fetch(`/users/${currentUser.id}/books`)
-//         .then(response => response.json())
-//         .then(data => setBooks(data))
-//         .catch(error => console.error(error));
-//     }
-//   }, [currentUser]);
-
-//   function handleRemoveFromCollection(bookId) {
-//     // send DELETE request to remove the book from the user's collection
-//     fetch(`/users/${currentUser.id}/books/${bookId}`, {
-//       method: 'DELETE',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         user_id: currentUser.id,
-//         book_id: bookId
-//       })
-//     })
-//       .then(response => response.json())
-//       .then(data => {
-//         // remove the book from the state
-//         setBooks(prevBooks => prevBooks.filter(book => book.id !== bookId));
-//         // show success alert
-//         Swal.fire({
-//           title: 'Removed from collection!',
-//           icon: 'success'
-//         });
-//       })
-//       .catch(error => console.error(error));
-//   }
-
-//   return (
-//     <div className="user-page">
-//       <nav className="user-navbar">
-//         <div className="user-avatar">
-//           <FaUserCircle size={32} />
-//           {currentUser && <span >Welcome {currentUser.name}!</span>}
-//         </div>
-//       </nav>
-//       <div className="user-collection">
-//         <h1 className="user-collection-title">My Collection</h1>
-//         <div className="user-book-cards">
-//           {books.map(book => (
-//             <div key={book.id} className="user-book-card">
-//               <img src={book.image_url} alt={book.title} className="user-book-image" />
-//               <h2 className="user-book-title">{book.title}</h2>
-//               <button className="user-book-remove-button" onClick={() => handleRemoveFromCollection(book.id)}>
-//                 Remove from Collection
-//               </button>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default User;
-
 import { useState, useEffect } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import Swal from 'sweetalert2';
@@ -87,7 +6,13 @@ import './User.css';
 function User() {
   const [currentUser, setCurrentUser] = useState(null);
   const [books, setBooks] = useState([]);
-       
+  const [favorites, setFavorites] = useState([]);    
+
+  function addFavorite(book) {
+    setFavorites([...favorites, book]);
+  }
+
+
   const token =localStorage.getItem('token');
   console.log('token ', token);  
   useEffect(() => {
@@ -114,6 +39,8 @@ function User() {
   const user= currentUser?.current_user.books.map((book, index) =>{
     
     return(
+
+      
       <div key = {index}className="user-book-card" >
         <h2 className="user-book-title">{book.title} </h2>
         <img src={book.image_url} alt={book.title} className="user-book-image" />
@@ -126,23 +53,24 @@ function User() {
       
   } )
 
-  function handleRemoveFromCollection(bookId) {
+  function handleRemoveFromCollection(id) {
     // send DELETE request to remove the book from the user's collection
-    fetch(`/users/${currentUser.id}/books/${bookId}`, {
+    fetch(`currentUser?.current_user.books/${id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        user_id: currentUser.id,
-        book_id: bookId
+     
       })
-    })
+    
       .then(response => response.json())
       .then(data => {
         // remove the book from the state
-        setBooks(prevBooks => prevBooks.filter(book => book.id !== bookId));
-        // show success alert
+        setCurrentUser(prevUser => {
+          const updatedBooks = prevUser.current_user.books.filter(book => book.id !== id);
+          return { ...prevUser, current_user: { ...prevUser.current_user, books: updatedBooks } };
+        });
+          // show success alert
         Swal.fire({
           title: 'Removed from collection!',
           icon: 'success'
